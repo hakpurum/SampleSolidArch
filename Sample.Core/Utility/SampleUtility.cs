c2e673249b58e729394fc7e01d390a975b3e7f56
Hasan Akpürüm <hakpurum@gmail.com>
2016-05-01 16:43:48 +0300

    update

--- Sample.Core/Utility/SampleUtility.cs
+++ Sample.Core/Utility/SampleUtility.cs
@@ -11,25 +11,20 @@ namespace Sample.Core.Utility
     {
         public static T GetAppSetting<T>(string key, T defaultValue)
         {
-            if (!string.IsNullOrEmpty(key))
+            if (string.IsNullOrEmpty(key)) return defaultValue;
+            var value = ConfigurationManager.AppSettings[key];
+            try
             {
-                var value = ConfigurationManager.AppSettings[key];
-                try
-                {
-                    if (value != null)
-                    {
-                        var theType = typeof(T);
-                        if (theType.IsEnum)
-                            return (T)Enum.Parse(theType, value, true);
+                if (value == null) return default(T);
+                var theType = typeof(T);
+                if (theType.IsEnum)
+                    return (T)Enum.Parse(theType, value, true);
 
-                        return (T)Convert.ChangeType(value, theType);
-                    }
-
-                    return default(T);
-                }
-                catch
-                {
-                }
+                return (T)Convert.ChangeType(value, theType);
+            }
+            catch (Exception)
+            {
+                // ignored
             }
 
             return defaultValue;
